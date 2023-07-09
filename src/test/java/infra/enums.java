28f10e4fbb718635767c6c66804af2bf314b4338
firasNess <p0035254@ness-tech.co.il>
2023-07-09 11:47:35 +0300

    add enums

---
+++ src/test/java/infra/enums.java
@@ -0,0 +1,71 @@
+package infra;
+
+public class enums {
+    public enum UIStyle {
+        OLD(1),
+        NEW(2);
+
+        private int value;
+
+        private UIStyle(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public enum SystemUnderTest {
+        PROD(1),
+        PREPROD(2);
+
+        private int value;
+
+        private SystemUnderTest(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public enum WaitInterval {
+        FAST(1),
+        SHORT(5),
+        MEDIUM(10),
+        LONG(30),
+        TOO_LONG(180);
+
+        private int value;
+
+        private WaitInterval(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public enum StepNumber {
+        FIRST("app-first-step"),
+        SECOND("app-second-step"),
+        THIRD("app-third-step"),
+        FOURTH("app-fourth-step"),
+        FIFTH("app-fifth-step"),
+        SIXTH("app-sixth-step");
+
+        private String value;
+
+        private StepNumber(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+}
--- src/test/java/steps/JMChromeWebDriver.java
+++ src/test/java/steps/JMChromeWebDriver.java
@@ -7,9 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import packages.Context;
-
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+
 
 public class JMChromeWebDriver {
     private static WebDriver driver;
@@ -22,9 +21,20 @@ public class JMChromeWebDriver {
         if (context.getVariables("driver") == null) {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
+            if((boolean) context.getVariables("headless")){
+                options.setHeadless(true);
+                options.setExperimentalOption("useAutomationExtension", false);
+                options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36");
+
+            }
+            if((boolean) context.getVariables("private")){
+                options.addArguments("--incognito");
+            }
             options.addArguments("--remote-allow-origins=*");
             driver = new ChromeDriver(options);
             context.setVariables("driver", driver);
+            options.addArguments("--remote-debugging-port=9222");
+            options.addArguments("--no-proxy-server");
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
